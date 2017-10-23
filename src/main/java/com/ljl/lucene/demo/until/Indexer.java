f2a2d5aa16a494793666a58cd9fe92a39788eeec
li.jialun <858898485@qq.com>
2017-10-23 18:34:05 +0800

    各种查询

--- src/main/java/com/ljl/lucene/demo/until/Indexer.java
+++ src/main/java/com/ljl/lucene/demo/until/Indexer.java
@@ -1,91 +1,99 @@
-//package com.ljl.lucene.demo.until;
-//
-//import com.ljl.lucene.demo.constants.LuceneConstants;
-//import org.apache.lucene.analysis.standard.StandardAnalyzer;
-//import org.apache.lucene.document.Document;
-//import org.apache.lucene.document.Field;
-//import org.apache.lucene.index.CorruptIndexException;
-//import org.apache.lucene.index.IndexWriter;
-//import org.apache.lucene.store.Directory;
-//import org.apache.lucene.store.FSDirectory;
-//import org.apache.lucene.util.Version;
-//
-//import java.io.File;
-//import java.io.FileFilter;
-//import java.io.FileReader;
-//import java.io.IOException;
-//
-///**
-// * 用于索引的原始数据，这样我们就可以使用Lucene库，使其可搜索。
-// *
-// * @author lijialun
-// * @create 2017-10-20 16:41
-// **/
-//public class Indexer {
-//    private IndexWriter writer;
-//
-//    public Indexer(String indexDirectoryPath) throws IOException {
-//        //this directory will contain the indexes
-//        try {
-//        Directory indexDirectory =
-//                FSDirectory.open(new File(indexDirectoryPath));
-//
-//        //create the indexer
-//        writer = new IndexWriter(indexDirectory,
-//                new StandardAnalyzer(Version.LUCENE_36),true,
-//                IndexWriter.MaxFieldLength.UNLIMITED);
-//    } catch (IOException e) {
-//        e.printStackTrace();
-//    }
-//    }
-//
-//    public void close() throws CorruptIndexException, IOException{
-//        writer.close();
-//    }
-//
-//    private Document getDocument(File file) throws IOException{
-//        Document document = new Document();
-//
-//        //index file contents
-//        Field contentField = new Field(LuceneConstants.CONTENTS,
-//                new FileReader(file));
-//        //index file name
-//        Field fileNameField = new Field(LuceneConstants.FILE_NAME,
-//                file.getName(),
-//                Field.Store.YES,Field.Index.NOT_ANALYZED);
-//        //index file path
-//        Field filePathField = new Field(LuceneConstants.FILE_PATH,
-//                file.getCanonicalPath(),
-//                Field.Store.YES,Field.Index.NOT_ANALYZED);
-//
-//        document.add(contentField);
-//        document.add(fileNameField);
-//        document.add(filePathField);
-//
-//        return document;
-//    }
-//
-//    private void indexFile(File file) throws IOException{
-//        System.out.println("Indexing "+file.getCanonicalPath());
-//        Document document = getDocument(file);
-//        writer.addDocument(document);
-//    }
-//
-//    public int createIndex(String dataDirPath, FileFilter filter)
-//            throws IOException{
-//        //get all files in the data directory
-//        File[] files = new File(dataDirPath).listFiles();
-//
-//        for (File file : files) {
-//            if(!file.isDirectory()
-//                    && !file.isHidden()
-//                    && file.exists()
-//                    && file.canRead()
-//                    && filter.accept(file)
-//                    ){
-//                indexFile(file);
-//            }
-//        }
-//        return writer.numDocs();
-//    }
-//}
+package com.ljl.lucene.demo.until;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Paths;
+
+public class Indexer {
+
+	private IndexWriter writer; // 写索引实例
+
+	/**
+	 * 构造方法 实例化IndexWriter
+	 * @param indexDir
+	 * @throws Exception
+	 */
+	public Indexer(String indexDir)throws Exception{
+		Directory dir=FSDirectory.open(Paths.get(indexDir));
+		Analyzer analyzer=new StandardAnalyzer(); // 标准分词器
+		IndexWriterConfig iwc=new IndexWriterConfig(analyzer);
+		writer=new IndexWriter(dir, iwc);
+	}
+
+	/**
+	 * 关闭写索引
+	 * @throws Exception
+	 */
+	public void close()throws Exception{
+		writer.close();
+	}
+
+	/**
+	 * 索引指定目录的所有文件
+	 * @param dataDir
+	 * @throws Exception
+	 */
+	public int index(String dataDir)throws Exception{
+		File []files=new File(dataDir).listFiles();
+		for(File f:files){
+			indexFile(f);
+		}
+		return writer.numDocs();
+	}
+
+	/**
+	 * 索引指定文件
+	 * @param f
+	 */
+	private void indexFile(File f) throws Exception{
+		System.out.println("索引文件："+f.getCanonicalPath());
+		Document doc=getDocument(f);
+		writer.addDocument(doc);
+	}
+
+	/**
+	 * 获取文档，文档里再设置每个字段
+	 * @param f
+	 */
+	private Document getDocument(File f)throws Exception {
+		Document doc=new Document();
+		doc.add(new TextField("contents",new FileReader(f)));
+		doc.add(new TextField("fileName", f.getName(),Field.Store.YES));
+		doc.add(new TextField("fullPath",f.getCanonicalPath(),Field.Store.YES));
+		return doc;
+	}
+
+	public static void main(String[] args) {
+		String indexDir="D:\\lucene\\index";
+		String dataDir="D:\\lucene\\data";
+		Indexer indexer=null;
+		int numIndexed=0;
+		long start=System.currentTimeMillis();
+		try {
+			indexer = new Indexer(indexDir);
+			numIndexed=indexer.index(dataDir);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {
+				indexer.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		long end=System.currentTimeMillis();
+		System.out.println("索引："+numIndexed+" 个文件 花费了"+(end-start)+" 毫秒");
+	}
+}
